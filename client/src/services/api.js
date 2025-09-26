004e0202243b969e82e6ec90c4e5a2de38dfd8ab
nimrod kipngetich <nimrod.kipngetich@student.moringaschool.com>
2025-09-27 00:17:59 +0300

    update flask url on client side

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://job-board-app-avqf.onrender.com/api/v1';
+const API_BASE_URL = 'https://job-board-app-avqf.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
