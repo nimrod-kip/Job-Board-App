f6cd68d921fb344b9584835626b42e842a6609b3
nimrod kipngetich <nimrod.kipngetich@student.moringaschool.com>
2025-09-26 23:15:50 +0300

    update config to use postgres

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 import os
 # form models import db
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///jobboard.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
